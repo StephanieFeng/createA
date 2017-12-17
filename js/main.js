ae9253a484d7c0c371f8e617a8df30fb6bd50ee9
EricPan <pan.weifeng@live.cn>
2017-12-17 17:51:41 +0800

    add comments

--- js/main.js
+++ js/main.js
@@ -41,8 +41,38 @@ function saveComments() {
         "likeit": commentsFrom.likeit.value,
         "message": commentsFrom.message.value
     };
-    $.post("http://www.z2hacademy.cn/user/publish/W724-1501318062/comments/add/", data, function (data) {
+    $.post("http://www.z2hacademy.cn/user/publish/W984-1513502460/comments/add/", data, function (data) {
         alert('谢谢你的留言');
+        refreshComments();
     });
 
 }
+
+var pageSize = 10;
+var currentOffset = -1;
+function fillData(data) {
+    $('#page-content').empty();
+    if (data.total > 0) {
+        currentOffset = data.offset;
+        $("#totalCount").text(data.total);
+        data.data.forEach(function (element) {
+            $('#page-content').append("<div style='margin-bottom:20px;'><ul><li class='col-md-1 col-sm-1'></li>" +
+                "<li class='col-md-2 col-sm-2 col-xs-3'><img src='resource/people-icon.png'></li>" +
+                "<li class='col-md-8 col-sm-8 col-xs-8'' style='padding-top:8px;'><p>" + element.uname + "</p><span>" + element.comments +
+                "</span></li><li class='col-md-1 col-sm-1'></li><div class='clearfix'></div></ul>");
+        }, this);
+    } else {
+        $('#page-content').html('还没有留言哦!');
+        $("#totalCount").empty();
+    }
+}
+
+function refreshComments() {
+    $.get("http://www.z2hacademy.cn/user/publish/W984-1513502460/comments/list/",
+        { offset: 0, count: pageSize },
+        function (data) {
+            fillData(data);
+        });
+}
+
+$(refreshComments);
